4f4d4f1d999af90499a2a2847a1af371b478d1a8
Christopher Lee <christophermlee2@gmail.com>
2025-07-22 21:02:25 +0100

    inital commit

---
+++ src/main/java/com/choicespecs/e_commerce_proj_user_service/ECommerceProjUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.choicespecs.e_commerce_proj_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProjUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProjUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choicespecs/e_commerce_proj_user_service/ECommerceProjUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choicespecs.e_commerce_proj_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProjUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
